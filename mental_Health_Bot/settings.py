f497f88d4bf370c63bb7a54ae037181d127b871e
Manasesh K S A <129578069+ksamanasesh@users.noreply.github.com>
2025-01-20 18:29:55 +0530

    Update settings.py

--- mental_Health_Bot/settings.py
+++ mental_Health_Bot/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY', 'AIzaSyA4A3uF4Lma6QS7oJKyf7XfUIB7bfTYo8o')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
