a8406ae1d558d60dcce55f69572a517c997765db
ksamanasesh <manasesh2003@gmail.com>
2025-03-16 17:45:28 +0530

    b

--- mental_Health_Bot/settings.py
+++ mental_Health_Bot/settings.py
@@ -122,11 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static"),    
-]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Local static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collected files will go
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
